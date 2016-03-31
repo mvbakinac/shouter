94791456dccef48adad6b6e0615187031b5f78a6
milo vrach <morelo87@gmail.com>
2016-03-31 10:34:39 +0200

    Finished setup

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,10 +1,17 @@
 class ShoutsController < ApplicationController
   def create
   	shout = current_user.shouts.build(shouts_params)
-  	shout.save
-  	redirect_to dashboard_path
+  	if shout.save
+  	  redirect_to dashboard_path
+  	else
+  		flash.alert = "Nisam mogao da kriknem!"
+  		redirect_to dashboard_path
+  	end
   end
-
+  
+  def show
+    @shout = Shout.find(params[:id])
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
  
   root to: 'homes#show', via: :get
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resource :dashboard, only: [:show]
-  resources :shouts, only: [:create]
+  resources :shouts, only: [:create, :show]
 end

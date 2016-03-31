75d1e7e841922151018688ccb1eee38c1e11f6ff
milo vrach <morelo87@gmail.com>
2016-03-31 15:30:35 +0200

    Refaktoring

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
  
   root to: 'homes#show', via: :get
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :index] do
+  	post "follow" => "following_relationships#create"
+  	delete 'follow' => "following_relationships#destroy"
+  end
   resource :dashboard, only: [:show]
   resources :shouts, only: [ :show]
   resources :text_shouts, only: [:create]
